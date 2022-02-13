04883132060a944389598cd44315d0c75b99b3b1
lalli-oni <lalli.oni@gmail.com>
2022-02-13 22:30:42 +0100

    Create a Language Selector component

---
+++ src/LanguageSelector.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let languages = [];
+    export let language = languages[0];
+
+</script>
+
+<select bind:value={language}>
+    {#each languages as language}
+        <option value={language}>{language.emoji}</option>
+    {/each}
+</select>
