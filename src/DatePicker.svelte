1bb8911a097450a764ca5d37006c8c3b630868da
lalli-oni <lalli.oni@gmail.com>
2022-02-13 22:31:23 +0100

    Use events for DatePicker dateSelected

--- src/DatePicker.svelte
+++ src/DatePicker.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
     export let label: string;
     export let id: string;
 
-    export let date: Date;
-    let internalDate: Date;
+    function dateSelected(event) {
+        dispatch('date-selected', { id, date: event.target.valueAsDate });
+    };
 </script>
+
+<!-- {@debug date} -->
 
 <div class="flex flex-col">
     <label class="text-white" for={id}>{label}</label>
-    <input type="date" id={id} bind:value={internalDate} />
+    <input type="date" id={id} on:input={dateSelected} />
 </div>
