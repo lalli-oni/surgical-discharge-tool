88ea650a2091bab1f9ec122575a2ac3e55f96bdc
lalli-oni <lalli.oni@gmail.com>
2022-02-13 22:31:31 +0100

    Create Dropdown component

---
+++ src/Dropdown.svelte
@@ -0,0 +1,15 @@
+<script lang="ts" context="module">
+    export type DropdownOption = {
+        id: string;
+        label: string;
+    };
+
+    export let options: DropdownOption[];
+    export let selected: DropdownOption;
+</script>
+
+<select bind:value={selected}>
+    {#each options as option}
+        <option>{option.label}</option>
+    {/each}
+</select>
