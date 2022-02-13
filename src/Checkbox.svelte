33818e3e61f34db06d19fb1eb9b1bb41fa3268e3
lalli-oni <lalli.oni@gmail.com>
2022-02-13 22:31:03 +0100

    Style Check Box component

--- src/Checkbox.svelte
+++ src/Checkbox.svelte
@@ -3,7 +3,7 @@
     export let id: string;
 </script>
 
-<div class="flex flex-row">
-    <label class="text-white" for={id}>{label}</label>
+<div class="flex flex-row gap-4 items-baseline">
     <input type="checkbox" id={id}/>
+    <label class="text-white" for={id}>{label}</label>
 </div>
