8b3e770458b95e75e0c2f2a7567c041d803c84b9
lalli-oni <lalli.oni@gmail.com>
2022-02-13 22:33:27 +0100

    Update Form component Use translation tokens for labels Update date changed binding Dispatch event on selection-changed

--- src/Form.svelte
+++ src/Form.svelte
@@ -1,36 +1,48 @@
 <script lang="ts">
+    import { capitalizeFirstLetter } from './functions/text-mutators';
+	import { createEventDispatcher } from 'svelte';
+
+    export let labels;
+
+	const dispatch = createEventDispatcher();
+
     import Checkbox from "./Checkbox.svelte";
     import DatePicker from "./DatePicker.svelte";
 
-    // Form Values
-    let operationDate;
+    const dateChanged = (event) => {
+        console.log(event);
+        selection.operationDate = event.detail.date;
+        dispatch('selection-changed', selection);
+    };
 
+    const selection = {
+        organ: "heart",
+        operationDate: new Date(),
+    }
 </script>
 
 <form class="flex flex-col gap-4">
     <section>
-        <h2 class="text-white">Organ</h2>
+        <h2 class="text-white">{labels.organ}</h2>
         <select name="organs" id="organ-selection">
-            <option value="heart">Heart</option>
-            <option value="lungs">Lungs</option>
+            <option value="heart">{labels.heart}</option>
+            <option value="lungs">{labels.lungs}</option>
         </select>
     </section>
     <section>
-        <h2 class="text-white">Times</h2>
-        <DatePicker bind:operationDate label="Day of operation" id="operation-date"/>
+        <h2 class="text-white">{labels.times.label}</h2>
+        <DatePicker on:date-selected={dateChanged} label={labels.times.dayOfOperation} id="operation-date"/>
     </section>
     <section>
-        <h2 class="text-white">Heart rhytms</h2>
-        <Checkbox label="Sinus" id="sinus-rhytm" />
-        <Checkbox label="Chronical AF" id="chron-af" />
-        <Checkbox label="Paroxysmal AF" id="parox-af" />
-        <Checkbox label="New AF" id="new-af" />
-        <Checkbox label="Flutter" id="flutter-af" />
-
-        
+        <h2 class="text-white">{labels.rhytms.label}</h2>
+        <Checkbox label={labels.rhytms.sinus} id="sinus-rhytm" />
+        <Checkbox label={labels.rhytms.chronicalAF} id="chron-af" />
+        <Checkbox label={labels.rhytms.paroxysmalAF} id="parox-af" />
+        <Checkbox label={labels.rhytms.newAF} id="new-af" />
+        <Checkbox label={labels.rhytms.flutter} id="flutter-af" />
     </section>
     <section>
-        <h2 class="text-white">Respiratory</h2>
+        <h2 class="text-white">{labels.respitory.label}</h2>
         <Checkbox label="No problems" id="" />
 
     </section>
