ecd5c7e8ccf7d39e01006d1ad116bda11deaeeb5
lalli-oni <lalli.oni@gmail.com>
2022-02-16 21:12:16 +0100

    Format DateTime output text

--- src/hooks/useTextGenerator.ts
+++ src/hooks/useTextGenerator.ts
@@ -1,10 +1,19 @@
+
+
 export function useTextGenerator() {
 	const generate = (selections, translation) => {
+		console.log(selections);
 		let text = translation.output.pattern;
 		const tokens = [...text.matchAll(/(?:\{\{)(.+?)(?:\}\})/g)];
 		tokens.forEach(token => {
-			console.log(token);
-			text = text.replaceAll(token[0], selections[token[1]]);
+			let value: string;
+			if (selections[token[1]] instanceof Date) {
+				value = `${selections[token[1]].getDate()}.${selections[token[1]].getMonth() + 1}.${selections[token[1]].getYear()}`;
+			}
+			else {
+				value = selections[token[1]];
+			}
+			text = text.replaceAll(token[0], value);
 		});
 		return text
 	};
