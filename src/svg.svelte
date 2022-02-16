cc765b57021d925539099c2743277cf22599d627
lalli-oni <lalli.oni@gmail.com>
2022-02-16 21:13:11 +0100

    Create svg component

---
+++ src/svg.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let label: string;
+    export let width: string = "1rem";
+    export let height: string = "1rem";
+    export let box: number = 32;
+    export let icon: string;
+    
+</script>
+
+<svg class={$$props.class} aria-label={label} {width} {height} viewBox="0 0 {box} {box}"
+>{@html icon}</svg>
