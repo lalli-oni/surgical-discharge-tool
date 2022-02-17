3ad0048e786d9b57530a90ae7948d21b8ff2ced0
lalli-oni <lalli.oni@gmail.com>
2022-02-17 16:50:32 +0100

    Bind checkbox checked value

--- src/Checkbox.svelte
+++ src/Checkbox.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     export let label: string;
-    export let id: string;
+    export let id: string = null;
+    export let checked: boolean;
 </script>
 
 <div class="flex flex-row gap-4 items-baseline">
-    <input type="checkbox" id={id}/>
+    <input bind:checked type="checkbox" {id} />
     <label class="text-white" for={id}>{label}</label>
 </div>
