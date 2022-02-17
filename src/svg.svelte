cf3614c76c58bc6f0ac133903ae103be42409c2e
lalli-oni <lalli.oni@gmail.com>
2022-02-17 16:53:21 +0100

    Add click handler to svg component

--- src/svg.svelte
+++ src/svg.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+    export let id: string;
     export let label: string;
     export let width: string = "1rem";
     export let height: string = "1rem";
-    export let box: number = 32;
+    export let boxWidth: number = 32;
+    export let boxHeight: number = 32;
     export let icon: string;
-    
+
+    const onClick = (event) => {
+        dispatch('svg-click', id);
+    };
 </script>
 
-<svg class={$$props.class} aria-label={label} {width} {height} viewBox="0 0 {box} {box}"
+<svg class={$$props.class} {id} on:click={onClick} aria-label={label} {width} {height} viewBox="0 0 {boxWidth} {boxHeight}"
 >{@html icon}</svg>
