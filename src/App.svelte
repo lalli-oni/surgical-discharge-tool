f86a6b947b3d557cba6c6658e079956c47a015e1
lalli-oni <lalli.oni@gmail.com>
2022-02-17 16:53:54 +0100

    Remove hard coded language options Fix bindings Update styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import english from "./texts/english";
-	import pirate from "./texts/pirate";
+	import * as languages from "./texts";
 	import { useTextGenerator } from "./hooks/useTextGenerator";
 
 	import Form from "./Form.svelte";
@@ -10,41 +9,25 @@
 	const textGenerator = useTextGenerator();
 	const selectionChanged = (event) => {
 		selection = event.detail;
-		if (selection && translations) outputText = textGenerator.generate(selection, translations);
-	};
-	const languageChanged = (event) => {
-		if (selection && translations) outputText = textGenerator.generate(selection, translations);
+		if (selection && language) outputText = textGenerator.generate(selection, language);
 	};
 
-	$: if (language) translations = (language === "english") ? english : pirate;
+	let language = languages.english;
 
-	$: if (selection) console.log(selection)
-	const languages = [{
-		id: "english",
-		emoji: "👑",
-		svg:`<svg xmlns="http://www.w3.org/2000/svg" id="flag-icons-no" viewBox="0 0 640 480"><path fill="#ed2939" d="M0 0h640v480H0z"/><path fill="#fff" d="M180 0h120v480H180z"/><path fill="#fff" d="M0 180h640v120H0z"/><path fill="#002664" d="M210 0h60v480h-60z"/><path fill="#002664" d="M0 210h640v60H0z"/></svg>`
-	}, {
-		id: "pirate",
-		emoji: "🏴‍☠️",
-		svg:`<svg xmlns="http://www.w3.org/2000/svg" id="flag-icons-no" viewBox="0 0 640 480"><path fill="#ed2939" d="M0 0h640v480H0z"/><path fill="#fff" d="M180 0h120v480H180z"/><path fill="#fff" d="M0 180h640v120H0z"/><path fill="#002664" d="M210 0h60v480h-60z"/><path fill="#002664" d="M0 210h640v60H0z"/></svg>`
-
-	}];
-	let language;
 	let selection;
-	let translations = english;
 	let outputText = "";
 </script>
 
-<div class="bg-blue-900 h-screen py-8 px-16 flex flex-col">
+<div class="bg-brand-primary h-screen py-8 px-16 flex flex-col">
 	<div class="grow-0 mb-8 flex flex-row justify-between items-center">
 		<div>
-			<h1 class="text-blue-100 font-semibold text-3xl">{translations.title}</h1>
-			<div class="text-blue-300 text=xl">{translations.subtitle}</div>
+			<h1 class="text-blue-100 font-semibold text-3xl">{language.title}</h1>
+			<div class="text-blue-300 text=xl">{language.subtitle}</div>
 		</div>
-		<LanguageSelector bind:language languages={languages} on:language-changed={languageChanged}/>
+		<LanguageSelector bind:language languages={Object.values(languages)} />
 	</div>
 	<main class="flex flex-row gap-8 grow justify-evenly">
-		<Form on:selection-changed={selectionChanged} labels={translations} />
+		<Form on:selection-changed={selectionChanged} labels={language} />
 		<TextOutput text={outputText} />
 	</main>
 </div>
