78d15feebc35b6803c17c7ce31ac5a5d86456f52
lalli-oni <lalli.oni@gmail.com>
2022-02-17 16:49:32 +0100

    Bug fix: get correct year Add rythm text generation Add repitory text generation

--- src/hooks/useTextGenerator.ts
+++ src/hooks/useTextGenerator.ts
@@ -2,19 +2,26 @@
 
 export function useTextGenerator() {
 	const generate = (selections, translation) => {
-		console.log(selections);
 		let text = translation.output.pattern;
 		const tokens = [...text.matchAll(/(?:\{\{)(.+?)(?:\}\})/g)];
 		tokens.forEach(token => {
 			let value: string;
 			if (selections[token[1]] instanceof Date) {
-				value = `${selections[token[1]].getDate()}.${selections[token[1]].getMonth() + 1}.${selections[token[1]].getYear()}`;
+				value = `${selections[token[1]].getDate()}.${selections[token[1]].getMonth() + 1}.${selections[token[1]].getFullYear()}`;
 			}
 			else {
 				value = selections[token[1]];
 			}
 			text = text.replaceAll(token[0], value);
 		});
+
+		for (const [key, value] of Object.entries(selections.rythms)) {
+			if (value === true && translation.output[key]) text += "\n" + translation.output[key];
+		}
+		
+		for (const [key, value] of Object.entries(selections.respitory)) {
+			if (value === true && translation.output[key]) text += "\n" + translation.output[key];
+		}
 		return text
 	};
 	return { generate };
