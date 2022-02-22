172d26506f479be85cd109122603cabc3322c58d
lalli-oni <lalli.oni@gmail.com>
2022-02-22 13:12:03 +0100

    Create single choice input component

---
+++ src/SingleChoice.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let groupName: string;
+    export let choices: Array<any> = [];
+    export let selected: any = null;
+</script>
+
+<div class="flex flex-col">
+    {#each choices as choice}
+        <label class="text-white">
+            <input type=radio value={choice.id} bind:group={selected} name={groupName} />
+            {choice.label}
+        </label>
+    {/each}
+</div>
+
