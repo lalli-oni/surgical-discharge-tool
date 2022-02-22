5987212238442768a737c88d8f5036d88e17404f
lalli-oni <lalli.oni@gmail.com>
2022-02-22 14:19:54 +0100

    Fix hover visibility issues

--- src/LanguageSelector.svelte
+++ src/LanguageSelector.svelte
@@ -1,47 +1,46 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
+    import { fade, fly } from 'svelte/transition';
     import Svg from './svg.svelte';
     export let languages;
     export let language = languages.english;
     let expanded: boolean = false;
-    export let collapseDelay: number = 300;
-
-	const dispatch = createEventDispatcher();
 
     const languageSelected = (event) => {
         const temp = languages.find((l) => l.meta.id === event.detail);
         if (temp) language = temp;
+        expanded = false;
     };
 
-    const triggerExpand = () => {
-        expanded = true;
+    const toggleVisibility = () => {
+        expanded = !expanded;
     }
-
-	$: if (expanded) {
-		window.setTimeout(() => {expanded = false}, collapseDelay);
-	}
 </script>
 
 
 <div
     class="flex flex-row justify-between items-center hover:cursor-pointer gap-2 group p-2"
-    on:click={triggerExpand} on:mouseenter={triggerExpand} on:click={triggerExpand}
+    on:click={toggleVisibility} on:mouseenter={toggleVisibility} on:mouseleave={toggleVisibility}
 >
     <div>
         <Svg id="selected-language" icon={language.meta.flag} label={language.id} class="group-hover:shadow-orange-glow" 
         boxWidth={640} boxHeight={480}
     />
-        <div class="hidden absolute group-hover:flex mt-3 p-2 flex-col bg-brand-lighter rounded-lg">
-            {#each languages.filter(lang => lang.meta.id !== language.meta.id) as languageOption}
-                <Svg class="shadow-md hover:shadow-blue-100 m-1"
-                    icon={languageOption.meta.flag}
-                    id={languageOption.meta.id}
-                    label={languageOption.meta.id}
-                    boxWidth={640} boxHeight={480}
-                    on:svg-click={languageSelected}
-                />
-            {/each}
-        </div>
+        {#if expanded === true}
+            <div
+                class="flex absolute mt-3 p-2 flex-col bg-brand-lighter rounded-lg"
+                in:fly="{{ y: -100, duration: 800 }}" out:fade={{ duration: 300 }}
+            >
+                {#each languages.filter(lang => lang.meta.id !== language.meta.id) as languageOption}
+                    <Svg class="shadow-md hover:shadow-blue-100 m-1"
+                        icon={languageOption.meta.flag}
+                        id={languageOption.meta.id}
+                        label={languageOption.meta.id}
+                        boxWidth={640} boxHeight={480}
+                        on:svg-click={languageSelected}
+                    />
+                {/each}
+            </div>
+        {/if}
     </div>
     <div class="text-white select-none">
         V
