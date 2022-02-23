08aac6ff367be61ecfdf638e88ebef9825bb879f
lalli-oni <lalli.oni@gmail.com>
2022-02-23 12:20:25 +0100

    Update background styling

--- src/App.svelte
+++ src/App.svelte
@@ -18,18 +18,20 @@
 	let outputText = "";
 </script>
 
-<div class="h-screen p-4 md:p-6 md:px-8 flex flex-col">
-	<div class="grow-0 mb-6 md:mb-8 flex flex-row justify-between items-center">
-		<div>
-			<h1 class="text-blue-100 font-semibold text-3xl">{language.title}</h1>
-			<div class="text-blue-300 text=xl">{language.subtitle}</div>
+<div class="h-full w-full bg-pattern">
+	<div class="p-4 md:p-6 md:px-8 flex flex-col">
+		<div class="grow-0 mb-6 md:mb-8 flex flex-row justify-between items-center">
+			<div>
+				<h1 class="text-blue-100 font-semibold text-3xl">{language.title}</h1>
+				<div class="text-blue-300 text=xl">{language.subtitle}</div>
+			</div>
+			<LanguageSelector bind:language languages={Object.values(languages)} />
 		</div>
-		<LanguageSelector bind:language languages={Object.values(languages)} />
+		<main class="flex flex-row gap-8 grow justify-evenly">
+			<Form on:selection-changed={selectionChanged} labels={language} />
+			<TextOutput text={outputText} />
+		</main>
 	</div>
-	<main class="flex flex-row gap-8 grow justify-evenly">
-		<Form on:selection-changed={selectionChanged} labels={language} />
-		<TextOutput text={outputText} />
-	</main>
 </div>
 
 <style global>
@@ -39,5 +41,11 @@
 
 	body {
 		@apply bg-brand-primary;
+	}
+
+	.bg-pattern {
+		background: linear-gradient(to bottom right, transparent, black 80%), url("../background.svg");
+		background-attachment: fixed;
+		background-size: cover;
 	}
 </style>
