9c1a41d0137e5af298a4101c3a63d17ba98fea4f
lalli-oni <lalli.oni@gmail.com>
2022-03-11 12:37:35 +0100

    Bug fix: add internal DatePicker value Update DatePicker styling

--- src/DatePicker.svelte
+++ src/DatePicker.svelte
@@ -6,6 +6,8 @@
     export let label: string;
     export let id: string;
 
+    let value;
+
     function dateSelected(event) {
         dispatch('date-selected', { id, date: event.target.valueAsDate });
     };
@@ -13,7 +15,7 @@
 
 <!-- {@debug date} -->
 
-<div class="flex flex-row justify-between gap-2">
+<div class="flex flex-row justify-between gap-2 items-baseline">
     <label class="text-white grow" for={id}>{label}</label>
-    <input type="date" id={id} on:input={dateSelected} class={`bg-transparent text-white border shrink ${(value) ? "border-4" : "border-2"}`} />
+    <input type="date" id={id} bind:value on:input={dateSelected} class={`bg-transparent max-w-60% rounded-md text-white p-2 border border-brand-primary hover:border-highlight-primary shrink ${(value) ? "border-2" : "border"}`} />
 </div>
