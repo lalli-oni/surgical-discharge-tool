d50eb46d9b1e950ea9621f150c275ff97fe7b902
lalli-oni <lalli.oni@gmail.com>
2022-03-11 12:38:32 +0100

    Update SingleChoice styling Use proper check mark svg

--- src/SingleChoice.svelte
+++ src/SingleChoice.svelte
@@ -3,16 +3,20 @@
     export let choices: Array<any> = [];
     export let selected: any;
 
+    import CheckMark from "./CheckMark.svelte";
+
     const select = (choice) => selected = choices.find(c => c.id === choice);
+
+    $: if (typeof selected === "string") selected = choices.find(c => c?.id === selected);
 </script>
 
 {#each choices as choice}
     <div
         on:click={(e) => select(choice?.id)}
         class={[
-            `border-2`,
-            `flex ${(choice?.icon?.length) ? "flex-col justify-evenly" : "flex-row justify-start"} items-center `,
-            `${(choice?.id === selected?.id) ? "border-highlight-primary" : "border-neutral-400"}`,
+            `border  hover:border-highlight-secondary`,
+            `flex gap-2 ${(choice?.icon?.length) ? "flex-col justify-evenly" : "flex-row justify-start"} items-center `,
+            `${(choice?.id === selected?.id) ? "border-highlight-primary" : "border-brand-primary"}`,
             `p-2 rounded-lg cursor-pointer`,
             `${(choice?.id === selected?.id) ? "stroke-highlight-primary" : "stroke-white"}`,
             `${(choice?.id === selected?.id) ? "text-highlight-primary" : "text-white"}`
@@ -21,7 +25,9 @@
         {#if choice?.icon?.length}
             {@html choice.icon}
         {:else}
-            <div class={`pr-4 ${(choice?.id === selected?.id)}`}>O</div>
+        <!-- TODO: Proper circle icon -->
+            <!-- <div class={`pr-4 ${(choice?.id === selected?.id)}`}>O</div> -->
+            <CheckMark checked={choice?.id === selected?.id} />
         {/if}
         <span class={`text-center`} for={choice.id}>
             {choice.label}
