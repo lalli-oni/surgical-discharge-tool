d16f32e1554f0b6f9adf8fd3dc766140fbfc1426
lalli-oni <lalli.oni@gmail.com>
2022-03-25 14:08:54 +0100

    Update AboutContent Extract style rules Improve img spacing Make names semibold

--- src/components/AboutContent.svelte
+++ src/components/AboutContent.svelte
@@ -1,34 +1,44 @@
-<section class="p-2 flex flex-col gap-12 cursor-default">
+<section class="flex flex-col gap-12 cursor-default pr-8">
     <div class="flex flex-row gap-4 group">
-    <img src="photos/ulfur-profile.png" alt="Ulfur profile pic" class="w-16 h-16 rounded-full border border-transparent group-hover:border-highlight-primary" />
+    <img src="photos/ulfur-profile.png" alt="Ulfur profile pic" />
         <div>
-            <span class="text-highlight-primary text-xl mb-2 mr-2">Úlfur</span>
+            <span class="text-highlight-primary text-xl font-semibold mb-2 mr-2">Úlfur</span>
             <span class="text-neutral-300 text-lg">Developer, Workplace Medic</span>
             <p class="group-hover:text-highlight-light cursor-default">Úlfur 'Nice' is considered a god by some tribes in the rural parts of the Scottish Higherlands. A man of mystery and mythical status, the Dalai Lama is to have said of him "His hugs, so warm and full of compassion to me as a human being. The exchange gave me an erection. An erection of love.".</p>
         </div>
     </div>
     <div class="flex flex-row gap-4 group">
         <div>
-            <span class="text-highlight-primary text-xl mb-2 mr-2">Larus Thor</span>
+            <span class="name">Larus Thor</span>
             <span class="text-neutral-300 text-lg">Developer</span>
             <p class="group-hover:text-highlight-light cursor-default">Larus Thor grew up licking unsuspecting peoples elbows in the supermarket. Already at the age of 6 labelled a XZ class weirdo by experts from around the Polynesian peninsula.</p>
         </div>
-        <img src="photos/larus-thor-silly.png" alt="Larus Thor profile pic" class="w-16 h-16 rounded-full border border-transparent group-hover:border-highlight-primary" />
+        <img src="photos/larus-thor-silly.png" alt="Larus Thor profile pic" />
     </div>
     <div class="flex flex-row gap-4 group">
-        <img src="photos/hilmar-profile.png" alt="Hilmar profile pic" class="w-16 h-16 rounded-full border border-transparent group-hover:border-highlight-primary" />
+        <img src="photos/hilmar-profile.png" alt="Hilmar profile pic"/>
         <div>
-            <span class="text-highlight-primary text-xl mb-2 mr-2">Hilmar Thor</span>
+            <span class="name">Hilmar Thor</span>
             <span class="text-neutral-300 text-lg">Graphic Design</span>
             <p class="group-hover:text-highlight-light cursor-default">Hilmar 'Babyface' the First has expressed the desire to remove my eye balls via my birth canal if I write anything weird about him. So all I say is that he is a stand up guy! And even when sitting or laying down, he is a fantastic individual. If you see him, give him a hug!</p>
         </div>
     </div>
     <div class="flex flex-row gap-4 group">
         <div>
-            <span class="text-highlight-primary text-xl mb-2 mr-2">Alechandrina</span>
+            <span class="name">Alechandrina</span>
             <span class="text-neutral-300 text-lg">Developer, Consultant</span>
             <p class="group-hover:text-highlight-light ">Not much is confirmed of this shadowy figure. Originating from somewhere in the North of South America, she has confirmed kills all over the globe. Ruthless and unyielding and will stop at nothing until the job is completed without fault. Known victims: Maduro's pet parrot, Kevin Spacey's gerbil and Harvey Weinstein's pet rock.</p>
         </div>
-        <img src="photos/alecha-profile.png" alt="Alecha profile pic" class="w-16 h-16 rounded-full border border-transparent group-hover:border-highlight-primary" />
+        <img src="photos/alecha-profile.png" alt="Alecha profile pic" />
     </div>
 </section>
+
+<style lang="postcss">
+    section img {
+        @apply w-16 h-16 mt-4 rounded-full border border-white group-hover:border-highlight-primary;
+    }
+
+    section .name {
+        @apply text-highlight-primary text-xl font-semibold mb-2 mr-2;
+    }
+</style>
