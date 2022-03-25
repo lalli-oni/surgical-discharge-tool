9c092fb729ccf50d878437f262f9b3711afc34fa
lalli-oni <lalli.oni@gmail.com>
2022-03-25 14:10:17 +0100

    Update Modal Add scrollbar styling Move close button from top of scrollbar

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -440,7 +440,7 @@
             {/if}
           {/if}
           <div
-            class={state.classContent}
+            class={(state.classContent) ? state.classContent + " m-4" : "m-4"}
             class:content={!unstyled}
             style={cssContent}
           >
@@ -500,7 +500,7 @@
       position: absolute;
       z-index: 1000;
       top: 1rem;
-      right: 1rem;
+      right: 3rem;
       margin: 0;
       padding: 0;
       width: 1.5rem;
@@ -536,37 +536,55 @@
       transform: translate(0, -50%) rotate(45deg);
       left: 0.25rem;
     }
-  
+
     .close:after {
       -webkit-transform: translate(0, -50%) rotate(-45deg);
       -moz-transform: translate(0, -50%) rotate(-45deg);
       transform: translate(0, -50%) rotate(-45deg);
       left: 0.25rem;
-    }
-  
+    } 
+
     .close:hover {
       background: black;
     }
-  
+
     .close:hover:before,
     .close:hover:after {
       height: 2px;
       background: white;
     }
-  
+
     .close:focus {
       border-color: #3399ff;
       box-shadow: 0 0 0 2px #3399ff;
     }
-  
+
     .close:active {
       transform: scale(0.9);
     }
-  
+
     .close:hover,
     .close:focus,
     .close:active {
       outline: none;
     }
-  </style>
-  
+
+    ::-webkit-scrollbar {
+      width: 1em;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+      background-color: white;
+    }
+
+    ::-webkit-scrollbar-track {
+      border-radius: 10px;
+      box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    }
+
+    ::-webkit-scrollbar-thumb {
+      border-radius: 10px;
+      background-color: darkgrey;
+      outline: 1px solid slategrey;
+    }
+</style>
